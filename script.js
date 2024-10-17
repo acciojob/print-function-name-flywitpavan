78ac6d7ec25cdb4829778d1b2aadc87edb415992
PAVAN SHIRSATH <158544111+flywitpavan@users.noreply.github.com>
2024-10-17 22:49:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+function functionName() {
+  alert(arguments.callee.name); // This will alert the name of the function
+}
+
+// Example of calling the function
+functionName();  // This will show an alert with "functionName"
+functionName();  // Alerts "functionName"
